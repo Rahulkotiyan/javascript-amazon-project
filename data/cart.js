e95aa64235a46798a1ddcf7cc5003605cbc6d633
Rahulkotiyan <143371571+Rahulkotiyan@users.noreply.github.com>
2025-07-27 00:19:06 +0530

    delete product from cart

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [];
+export let cart = [];
 
 export function addToCart(productId) {
   let matchingItem;
@@ -16,4 +16,15 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+}
+
+export function removeFromCart(productId){
+  const newCart =[];
+  cart.forEach((cartItem)=>{
+    if(cartItem.productId !== productId){
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
 }
