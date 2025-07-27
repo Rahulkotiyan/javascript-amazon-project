5b3a67576c2308c368904e6ceeaa44033f5d748d
Rahulkotiyan <143371571+Rahulkotiyan@users.noreply.github.com>
2025-07-28 01:03:17 +0530

    update

--- data/products.js
+++ data/products.js
@@ -1,3 +1,5 @@
+import {formatCurrency} from "../scripts/utils/money.js";
+
 export function getProduct(productId){
   let matchingProduct;
 
@@ -9,22 +11,45 @@ export function getProduct(productId){
   return matchingProduct;
 }
 
-export const products = [
-  {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090,
-    keywords: [
-      "socks",
-      "sports",
-      "apparel"
-    ]
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsUrl(){
+  return  `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice(){
+    `$${formatCurrency(this.priceCents)}`;
+  }
+}
+
+/*const product1 = new Product({
+  id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+  image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+  name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+  rating: {
+    stars: 4.5,
+    count: 87,
   },
+  priceCents: 1090,
+  keywords: ["socks", "sports", "apparel"],
+});
+console.log(product1);*/
+
+
+export const products = [
   {
     id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     image: "images/products/intermediate-composite-basketball.jpg",
@@ -668,4 +693,8 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails)=>{
+  new Product(productDetails);
+});
+
+console.log(products);
