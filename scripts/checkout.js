c854eacf1c13a3efe375d78ca1ecbead33ce94f9
Rahulkotiyan <143371571+Rahulkotiyan@users.noreply.github.com>
2025-07-28 18:31:19 +0530

    usage of fetch()

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,16 +1,12 @@
 import { renderOrderSummary } from "./checkouts/orderSummary.js";
 import { renderPaymentSummary } from "./checkouts/paymentSummary.js";
-import { loadProducts } from "../data/products.js";
+import { loadProductsFetch } from "../data/products.js";
 //import '../data/cart-oop.js';
 //import '../data/backend-practice.js';
 import { loadCart } from "../data/cart.js";
 
 Promise.all([
-  new Promise((resolve) => {
-    loadProducts(() => {
-      resolve("value1");
-    });
-  }),
+  loadProductsFetch(),
 
     new Promise((resolve)=>{
         loadCart(()=>{
