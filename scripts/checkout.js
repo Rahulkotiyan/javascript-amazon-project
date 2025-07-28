957e7b658dbd5c1fcbed5d82050b35e99158016a
Rahulkotiyan <143371571+Rahulkotiyan@users.noreply.github.com>
2025-07-28 15:58:55 +0530

    add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 import { renderOrderSummary } from "./checkouts/orderSummary.js";
 import { renderPaymentSummary } from "./checkouts/paymentSummary.js";
 //import '../data/cart-oop.js';
-
+import '../data/backend-practice.js';
 
 renderOrderSummary();
 renderPaymentSummary();
