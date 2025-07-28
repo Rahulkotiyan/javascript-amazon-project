f9635820286fcfeb1d2e22d10ec39520148878d3
Rahulkotiyan <143371571+Rahulkotiyan@users.noreply.github.com>
2025-07-28 19:03:22 +0530

    promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,6 +5,25 @@ import { loadProductsFetch } from "../data/products.js";
 //import '../data/backend-practice.js';
 import { loadCart } from "../data/cart.js";
 
+async function loadPage(){
+    
+    await loadProductsFetch();
+
+  const value = await new Promise((resolve) => {
+      loadCart(() => {
+        resolve('value3');
+      });
+    });
+
+    renderOrderSummary();
+    renderPaymentSummary();
+
+   
+}
+loadPage()
+
+
+/*
 Promise.all([
   loadProductsFetch(),
 
@@ -18,6 +37,8 @@ Promise.all([
     renderOrderSummary();
     renderPaymentSummary();
 });
+*/
+
 
 /*
 new Promise((resolve)=>{
